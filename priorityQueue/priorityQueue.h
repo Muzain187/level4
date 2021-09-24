d976a2d9f37c2792dc0f2150293d456d2040b389
Muzain187 <muzain.ashraf@gmail.com>
2021-09-24 07:21:59 +0530

    added Max priority Queue in header file

--- priorityQueue/priorityQueue.h
+++ priorityQueue/priorityQueue.h
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+//Min heap
 class priorityQueue{
     vector<int> pq;
 
@@ -72,5 +74,88 @@ class priorityQueue{
 		}
 
 		return ans;
+    }
+};
+
+//Max heap
+#include<vector>
+class PriorityQueue_MaxHeap {
+    // Declare the data members here
+    vector<int> pq;
+
+   public:
+    PriorityQueue_MaxHeap() {
+        // Implement the constructor here
+    }
+
+    /**************** Implement all the public functions here ***************/
+
+    void insert(int element) {
+        // Implement the insert() function here
+        pq.push_back(element);
+        int childIndex = pq.size()-1;
+        
+        while(childIndex > 0){
+        	int parentIndex = (childIndex-1)/2;
+            
+            if(pq[childIndex] > pq[parentIndex]){
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp;
+            }
+            else
+                break;
+            childIndex = parentIndex;
+            // parentIndex = (childIndex-1)/2;
+        }
+    }
+
+    int getMax() {
+        // Implement the getMax() function here
+        if(isEmpty())
+            return 0;
+        return pq.at(0);
+    }
+
+    int removeMax() {
+        // Implement the removeMax() function here
+        
+        int ans = pq[0];
+        pq[0] = pq[pq.size()-1];
+        pq.pop_back();
+        
+        int pi=0;
+
+        int lci=(2*pi+1),rci=(2*pi+2);
+        while(lci < pq.size()){
+            int maxIndex = pi;
+            
+            if(pq[lci] > pq[maxIndex])
+                maxIndex = lci;
+            if(rci<pq.size() && pq[rci] >= pq[maxIndex])
+                maxIndex = rci;
+
+            if(pi == maxIndex)
+                break;
+            
+            int temp = pq[maxIndex];
+            pq[maxIndex] = pq[pi];
+            pq[pi] = temp;
+
+            pi = maxIndex;
+            lci=(2*pi)+1;
+        	rci=(2*pi)+2;
+        }
+        return ans;
+    }
+
+    int getSize() { 
+        // Implement the getSize() function here
+        return pq.size();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function here
+        return pq.size()==0;
     }
 };
